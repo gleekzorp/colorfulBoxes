54992006cba752442ca650d570d8b81d0ad56be4
gleekzorp <gleek.dan@gmail.com>
2019-06-23 18:26:39 -0600

    Enter appears stretch goal complete.  Swaps between PRESS and ENTER

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 // VARIABLES
 const hexBoxParent = document.querySelectorAll('.hex-box-wrapper');
-const hexBoxChildren = document.querySelectorAll('.hex-box')
+const hexBoxChildren = document.querySelectorAll('.hex-box');
+const pressEnterDiv = document.querySelector('#press-enter');
+
 
 // EVENT LISTENERS
-document.addEventListener('keypress', keyPressed)
+document.addEventListener('keypress', keyPressed);
+pressEnterDiv.addEventListener('mouseenter', pressEnterSwap);
+pressEnterDiv.addEventListener('mouseleave', pressEnterSwap);
 
 for (var i = 0; i < hexBoxParent.length; i++) {
   var childElement = hexBoxParent[i];
   childElement.addEventListener('mouseover', applyColor, false);
 }
+
 
 // FUNCTIONS
 function applyColor(event) {
@@ -21,7 +26,7 @@ function applyColor(event) {
 
 function randomHex() {
   var hex = [];
-  const hexList = ['a', 'b', 'c', 'd', 'e', 'f', 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const hexList = ['a', 'b', 'c', 'd', 'e', 'f', 1, 2, 3, 4, 5, 6, 7, 8, 9];
   
   for (let i = 0; i < 6; i++) {        
       hex.push(hexList[Math.floor(Math.random() * hexList.length)])
@@ -33,7 +38,11 @@ function keyPressed(event) {
   if (event.which == 13 || event.keyCode == 13) {
     for (var i = 0; i < hexBoxChildren.length; i++) {
       var childElement = hexBoxChildren[i];
-      childElement.style.backgroundColor = 'rgb(127, 139, 166)'
+      childElement.style.backgroundColor = 'rgb(127, 139, 166)';
     }
   }
 };
+
+function pressEnterSwap(event) {
+  event.type == 'mouseenter' ? pressEnterDiv.innerHTML = 'PRESS' : pressEnterDiv.innerHTML = 'ENTER';
+}
